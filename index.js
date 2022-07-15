8f72bf63945a5cd59778e52222e4a46c677a1855
Alexander Gochnour <alexandergochnour@gmail.com>
2022-07-14 20:40:38 -0500

    updates

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
-// add menu toggle to bars icon in nav bar
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-});
+import { Header, Nav, Main, Footer } from "./components";
+import * as store from "./store";
+import Navigo from "navigo";
+import { capitalize } from "lodash";
+
+const router = new Navigo("/");
+
+function render(state = store.Home) {
+  document.querySelector("#root").innerHTML = `
+    ${Header(state)}
+    ${Nav(store.Links)}
+    ${Main(state)}
+    ${Footer()}
+  `;
+
+  router.updatePageLinks();
+  afterRender(state);
+}
+
+function afterRender(state) {
+  // add menu toggle to bars icon in nav bar
+  document.querySelector(".fa-bars").addEventListener("click", () => {
+    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+  });
+  document.querySelectorAll("nav a").forEach((navLink) =>
+    navLink.addEventListener("click", (event) => {
+      event.preventDefault();
+      render(state[event.target.title]);
+    })
+  );
+}
+
+router
+  .on({
+    "/": () => render(),
+    ":view": (params) => {
+      let view = capitalize(params.data.view);
+      render(store[view]);
+    },
+  })
+  .resolve();
