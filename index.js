42738717d0ceee2a3a7258bb44d127027a12b041
Alexander Gochnour <alexandergochnour@gmail.com>
2022-07-20 21:40:22 -0500

    push

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import * as store from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
 import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
 const router = new Navigo("/");
 
@@ -13,48 +15,67 @@ function render(state = store.Home) {
     ${Main(state)}
     ${Footer()}
   `;
-
   router.updatePageLinks();
-  afterRender(state);
+  afterRender();
 }
 
-function afterRender(state) {
+function afterRender() {
   // add menu toggle to bars icon in nav bar
   document.querySelector(".fa-bars").addEventListener("click", () => {
     document.querySelector("nav > ul").classList.toggle("hidden--mobile");
   });
 }
+
 router.hooks({
   before: (done, params) => {
-    const view = params && params.data && params.data.view ? capitalize(params.data.view) : "Home";
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home";
 
     // Add a switch case statement to handle multiple routes
     switch (view) {
       case "Home": {
         axios
           .get(
-            `https://api.openweathermap.org/data/2.5/weather?appid=fbb30b5d6cf8e164ed522e5082b49064&q=st%20louis`
+            `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st%20louis`
           )
-          .then(response => {
-            const kelvinToFahrenheit = kelvinTemp =>
+          .then((response) => {
+            const kelvinToFahrenheit = (kelvinTemp) =>
               Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
-
             store.Home.weather = {};
             store.Home.weather.city = response.data.name;
-            store.Home.weather.temp = kelvinToFahrenheit(response.data.main.temp);
-            store.Home.weather.feelsLike = kelvinToFahrenheit(response.data.main.feels_like);
+            store.Home.weather.temp = kelvinToFahrenheit(
+              response.data.main.temp
+            );
+            store.Home.weather.feelsLike = kelvinToFahrenheit(
+              response.data.main.feels_like
+            );
             store.Home.weather.description = response.data.weather[0].main;
             done();
           })
-          .catch(err => console.log(err));
-          break;
+          .catch((err) => console.log(err));
+        break;
       }
-      default : {
+      case "Pizza": {
+        axios
+          .get(`${process.env.PIZZA_PLACE_API_URL}`)
+          .then((response) => {
+            store.Pizza.pizzas = response.data;
+            done();
+          })
+          .catch((error) => {
+            console.log("It puked", error);
+          });
+        break;
+      }
+      default: {
         done();
       }
     }
-  }
+  },
 });
+
 router
   .on({
     "/": () => render(),
