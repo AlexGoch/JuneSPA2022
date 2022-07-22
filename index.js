d1f04e2c28379287179a4d52a4d51105059a8736
Alexander Gochnour <alexandergochnour@gmail.com>
2022-07-21 21:01:38 -0500

    update

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ router.hooks({
           .get(
             `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st%20louis`
           )
-          .then((response) => {
-            const kelvinToFahrenheit = (kelvinTemp) =>
+          .then(response => {
+            const kelvinToFahrenheit = kelvinTemp =>
               Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
             store.Home.weather = {};
             store.Home.weather.city = response.data.name;
@@ -54,17 +54,17 @@ router.hooks({
             store.Home.weather.description = response.data.weather[0].main;
             done();
           })
-          .catch((err) => console.log(err));
+          .catch(err => console.log(err));
         break;
       }
       case "Pizza": {
         axios
           .get(`${process.env.PIZZA_PLACE_API_URL}`)
-          .then((response) => {
+          .then(response => {
             store.Pizza.pizzas = response.data;
             done();
           })
-          .catch((error) => {
+          .catch(error => {
             console.log("It puked", error);
           });
         break;
@@ -73,15 +73,15 @@ router.hooks({
         done();
       }
     }
-  },
+  }
 });
 
 router
   .on({
     "/": () => render(),
-    ":view": (params) => {
+    ":view": params => {
       let view = capitalize(params.data.view);
       render(store[view]);
-    },
+    }
   })
   .resolve();
